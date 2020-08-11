d860a82fd02d8c7464808a2f74c2a035fb2af964
MWest352 <MWest35285@gmail.com>
2020-08-11 11:50:33 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,81 @@
+//Calculator
+
+
+let getHistory = () => document.getElementById("history-value").innerText;
+
+let printHistory = (num) => document.getElementById("history-value").innerText=num;
+
+let getOutput = () => document.getElementById("output-value").innerText;
+
+let printOutput = num => num=="" ? document.getElementById("output-value").innerText=(num) :
+        document.getElementById("output-value").innerText=getFormattedNumber(num);
+
+function getFormattedNumber(num){
+  if(num=="-"){
+  return"";
+  }
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+      return value;
+} 
+
+reverseNumberFormat = num => Number(num.replace(/,/g,''));
+
+
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length; i++){
+operator[i].addEventListener('click', function(){
+if(this.id=="clear"){
+  printHistory("");
+  printOutput("");
+}
+
+else if(this.id=="backspace"){
+  var 
+  output=reverseNumberFormat(getOutput()).toString();
+  if(output){//If Output Has Value
+    output= output.substr(0,output.length-1);
+    printOutput(output);
+  }
+}
+
+else{
+  var output=getOutput();
+  var history=getHistory();
+  if(output==""&&history!="")
+    if(isNaN(history[history.length-1])){
+      history=history.substr(0,history.length-1);
+    }
+  if(output!="" || history!=""){
+    //condition? true:false
+    output= output==""?
+    output:reverseNumberFormat(output);
+     history=history+output;
+     if(this.id=="="){
+       var result=eval(history); 
+       printOutput(result);
+       printHistory("");
+      }
+      else{
+        history=history+this.id;
+        printHistory(history);
+        printOutput("");
+      }
+  
+  }
+}
+});
+}
+
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length; i++){
+number[i].addEventListener('click', function(){
+var output=reverseNumberFormat(getOutput());
+if(output!=NaN){//if output is number
+  output=output+this.id;
+  printOutput(output);
+    }
+
+});
+}
+ 
